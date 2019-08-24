7eaf118b93d7b4e3b28ae28d427c9a9c8069a694
andouyosiaki <yutori4dayo@gmail.com>
2019-08-24 14:42:25 +0900

    修正

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- footer -->
   <footer>
-    <small>Copyright (c)  <a href="https://twitter.com/float_top">＠float_top</a> All Rights Reserved.</small>
+    <small>Copyright (c) <a href="https://twitter.com/float_top">＠float_top</a> All Rights Reserved.</small>
   </footer>
  </body>
 </html>
